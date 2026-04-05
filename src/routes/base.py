facd62489cc05fb6b9c8c333cbdccb93c2d7f937
Github <github_user@mini-rag-deployment.43pnnehczlqennx0yqlfj5xtxa.wawx.internal.cloudapp.net>
2026-04-05 20:05:50 +0000

    test

--- src/routes/base.py
+++ src/routes/base.py
@@ -16,5 +16,5 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
     return {
         "app_name": app_name,
         "app_version": app_version,
-        "datatime": datetime.now().isstrftime("%Y-%m-%d %H:%M:%S")
+        "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
